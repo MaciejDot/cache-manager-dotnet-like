0b8b9d323a8885c0a94217c28958ca2fe6cd1844
Maciek_dot <maciejd0@op.pl>
2020-11-28 20:47:31 +0100

    updated readme.md

--- src/Store/IStore.ts
+++ src/Store/IStore.ts
@@ -1,7 +1,4 @@
 export interface IStore {
-  //alreadyNamespaced
-  //only need is entry fillter
-  // send indexedDb query
   getItem: (key: string) => Promise<unknown>
   exist: (key: string) => Promise<boolean>
   setItem: (key: string, item: unknown) => Promise<void>
